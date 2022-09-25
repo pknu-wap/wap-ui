91cd22b991f0b3677a23322adaa50d32fa104228
neko113 <alstn113@gmail.com>
2022-09-25 14:58:27 +0900

    Issue #9 feat : 기본 theme 및 type 설정

---
+++ packages/theme/palette.ts
@@ -0,0 +1,10 @@
+export const palette = {
+  white: '#ffffff',
+  gray: '#dfe6e9',
+  black: '#000000',
+  primary: '#0072F5',
+  secondary: '#7828C8',
+  success: '#17C964',
+  warning: '#F5A524',
+  error: '#F31260',
+} as const;
---
+++ packages/theme/types.ts
@@ -0,0 +1,6 @@
+export type NormalColorType =
+  | 'primary'
+  | 'success'
+  | 'secondary'
+  | 'warning'
+  | 'error';
