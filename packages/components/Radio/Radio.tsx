78b5a90b7c60deb951d88e32a3967fee60078a06
neko113 <alstn113@gmail.com>
2022-09-25 19:48:24 +0900

    Issue #21 feat : Radio Component 생성

---
+++ packages/components/Radio/Radio.tsx
@@ -0,0 +1,27 @@
+import { NormalColorType } from '../../theme/types';
+import { forwardRef, InputHTMLAttributes } from 'react';
+import * as S from './Radio.styles';
+import React from 'react';
+
+export interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  labelText?: string;
+  color?: NormalColorType;
+  labelColor?: boolean;
+}
+
+export const Radio = forwardRef<HTMLInputElement, Props>(function Radio(
+  { labelText = '', labelColor = false, color = 'primary', ...options },
+  ref,
+) {
+  return (
+    <S.RadioLabel>
+      <S.RadioInput type="radio" ref={ref} color={color} {...options} />
+      <S.RadioPoint color={color} />
+      <S.RadioText labelColor={labelColor} color={color}>
+        {labelText}
+      </S.RadioText>
+    </S.RadioLabel>
+  );
+});
+
+export default Radio;
