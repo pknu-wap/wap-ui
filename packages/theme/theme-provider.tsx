98c3d27e312ac3f28b00fd15dd95539c5e0bd416
neko113 <alstn113@gmail.com>
2022-09-25 16:47:10 +0900

    Issue #17 refactor : globalStyle & css reset 삭제

--- packages/theme/theme-provider.tsx
+++ packages/theme/theme-provider.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { ThemeProvider, ThemeProviderProps } from '@emotion/react';
 import lightTheme from './light-theme';
-import GlobalStyle from './global-style';
 
 interface Props extends ThemeProviderProps {}
 
 export const WapUIProvider = ({ children, theme = lightTheme }: Props) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
