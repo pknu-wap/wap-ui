32257a81f56bf75a7f533df0fd0a911ce023fc13
neko113 <alstn113@gmail.com>
2022-10-04 22:25:01 +0900

    feat: commitlint setting (#24)

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+// Commit Rules
+module.exports = {
+  $schema: 'https://json.schemastore.org/commitlintrc.json',
+  extends: ['@commitlint/config-conventional'],
+};
+// ex) feat: add new feature (#13)
+// ex) fix: fix Button styles (#15)
+// ex) docs: add new docs (#13) (#14)
+// ex) refactor: refactor Checkbox border (#11)
