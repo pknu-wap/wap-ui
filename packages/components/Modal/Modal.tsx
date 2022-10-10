bd257ae082daf0f122d71ba1d84445e10e464714
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-10-11 00:46:51 +0900

    feat: prevent scroll (#31)

--- packages/components/Modal/Modal.tsx
+++ packages/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './Modal.styles';
 
 export interface Props {
@@ -7,5 +7,19 @@ export interface Props {
 }
 
 export const Modal = ({ isOpen = false, onClose }: Props) => {
-  return <>{isOpen && <S.Overlay onClick={onClose} />}</>;
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    let visibleId: NodeJS.Timeout;
+    if (isOpen) {
+      setVisible(true);
+      document.body.style.overflow = 'hidden';
+    } else {
+      visibleId = setTimeout(() => setVisible(false), 300);
+      document.body.style.overflow = 'auto';
+    }
+    return () => clearTimeout(visibleId);
+  }, [isOpen]);
+
+  return <>{visible && <S.Overlay onClick={onClose} />}</>;
 };
