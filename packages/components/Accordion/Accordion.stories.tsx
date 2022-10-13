e05a81c5307bad730ba53f66dd186c045619b7a6
neko113 <alstn113@gmail.com>
2022-10-13 21:24:13 +0900

    feat: add accordion storybook (#30)

--- packages/components/Accordion/Accordion.stories.tsx
+++ packages/components/Accordion/Accordion.stories.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Accordion, Props } from './Accordion';
+import { ComponentMeta } from '@storybook/react';
+import { Accordion } from './Accordion';
+import { AccordionItem } from './AccordionItem';
 
 export default {
   title: 'Components/Accordion',
   component: Accordion,
 } as ComponentMeta<typeof Accordion>;
 
-const Template: ComponentStory<typeof Accordion> = (args: Props) => (
-  <Accordion {...args} />
+export const Default = () => (
+  <Accordion>
+    <AccordionItem label="Item 1">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
+      dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos?
+    </AccordionItem>
+    <AccordionItem label="Item 2">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
+      dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos?
+    </AccordionItem>
+    <AccordionItem label="Item 3">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
+      dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos?
+    </AccordionItem>
+  </Accordion>
 );
-
-export const Default = Template.bind({});
-
-Default.args = {
-  children: 'Accordion',
-};
