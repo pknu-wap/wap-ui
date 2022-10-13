82c5073429db13a8aa46e4e80d3bc1bfff9d5aae
neko113 <alstn113@gmail.com>
2022-10-13 21:53:30 +0900

    feat: accordion item label::before styles (#30)

--- packages/components/Accordion/AccordionItem/AccordionItem.styles.ts
+++ packages/components/Accordion/AccordionItem/AccordionItem.styles.ts
@@ -9,9 +9,17 @@ export const Root = styled.div`
 export const Label = styled.div`
   position: relative;
   padding: 10px;
+  font-size: ${common.size.md};
   background: ${common.color.cyan3};
   color: ${common.color.white};
   cursor: pointer;
+  &::before {
+    content: '+';
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    transform: translateY(-50%);
+  }
 `;
 
 export const Content = styled.div``;
