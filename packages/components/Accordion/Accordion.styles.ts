4d4b4690ea7b8b8565cb6cdfa5d9711093c8ac14
neko113 <alstn113@gmail.com>
2022-10-13 21:47:44 +0900

    feat: accordion item label styles (#30)

--- packages/components/Accordion/Accordion.styles.ts
+++ packages/components/Accordion/Accordion.styles.ts
@@ -3,6 +3,5 @@ import { common } from '../../theme/common';
 
 export const Root = styled.div`
   max-width: 800px;
-  padding: 10px 20px;
   background-color: ${common.color.gray2};
 `;
--- packages/components/Accordion/AccordionItem/AccordionItem.styles.ts
+++ packages/components/Accordion/AccordionItem/AccordionItem.styles.ts
@@ -1,7 +1,17 @@
 import styled from '@emotion/styled';
+import { common } from '../../../theme/common';
 
-export const Root = styled.div``;
+export const Root = styled.div`
+  position: relative;
+  padding: 10px 20px;
+`;
 
-export const Label = styled.div``;
+export const Label = styled.div`
+  position: relative;
+  padding: 10px;
+  background: ${common.color.cyan3};
+  color: ${common.color.white};
+  cursor: pointer;
+`;
 
 export const Content = styled.div``;
