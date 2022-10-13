7d9efbeb287ad41278f01ffc7e1b52dee7d7b680
neko113 <alstn113@gmail.com>
2022-10-13 21:23:36 +0900

    feat: accordion item component 구체화 (#30)

--- packages/components/Accordion/AccordionItem/AccordionItem.styles.ts
+++ packages/components/Accordion/AccordionItem/AccordionItem.styles.ts
@@ -1,3 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Root = styled.div``;
+
+export const Label = styled.div``;
+
+export const Content = styled.div``;
--- packages/components/Accordion/AccordionItem/AccordionItem.tsx
+++ packages/components/Accordion/AccordionItem/AccordionItem.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import * as S from './AccordionItem.styles';
 
 export interface Props {
+  label: string;
   children: React.ReactNode;
 }
 
-export const AccordionItem = ({ children }: Props) => {
-  return <S.Root>{children}</S.Root>;
+export const AccordionItem = ({ children, label }: Props) => {
+  return (
+    <S.Root>
+      <S.Label>{label}</S.Label>
+      <S.Content>{children}</S.Content>
+    </S.Root>
+  );
 };
