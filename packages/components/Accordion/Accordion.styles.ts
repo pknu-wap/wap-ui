1f22d5049b8be2b4815f8e78161d6b6477c534cd
neko113 <alstn113@gmail.com>
2022-10-13 21:41:47 +0900

    feat: accordion root styles (#30)

--- packages/components/Accordion/Accordion.styles.ts
+++ packages/components/Accordion/Accordion.styles.ts
@@ -1,3 +1,8 @@
 import styled from '@emotion/styled';
+import { common } from '../../theme/common';
 
-export const Root = styled.div``;
+export const Root = styled.div`
+  max-width: 800px;
+  padding: 10px 20px;
+  background-color: ${common.color.gray2};
+`;
