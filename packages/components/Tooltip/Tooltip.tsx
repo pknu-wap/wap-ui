07c97f00e06be2b71bebaee1738d13ecefec25d4
neko113 <alstn113@gmail.com>
2022-10-25 22:56:37 +0900

    feat: add tooltip props (#37)

--- packages/components/Tooltip/Tooltip.tsx
+++ packages/components/Tooltip/Tooltip.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { NormalColorType } from '../../theme/types';
 import * as S from './Tooltip.styles';
 
 export interface Props {
   children: React.ReactNode;
+  content: string;
+  color: NormalColorType;
+  placement: 'top' | 'bottom' | 'left' | 'right';
 }
+
+/**
+ * @todo 이거 참고하기!!
+ * @see w3schools https://www.w3schools.com/css/css_tooltip.asp
+ * @see nextui https://nextui.org/docs/components/tooltip
+ */
 
 export const Tooltip = ({ children }: Props) => {
   return <S.Root>{children}</S.Root>;
