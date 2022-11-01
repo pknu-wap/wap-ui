ffbf5402d3da60ef4d453d93c1f090a3c76fb251
neko113 <alstn113@gmail.com>
2022-11-01 23:52:05 +0900

    fix: useDidMoutEffect utils types (#30)

--- packages/hooks/useDidMountEffect.ts
+++ packages/hooks/useDidMountEffect.ts
@@ -1,18 +1,23 @@
-import { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 /**
- * @todo 나중에 타입 추가하기
  * useDidMountEffect는 컴포넌트가 마운트 된 후에만 effect를 실행합니다.
  * 즉 마운트 시에는 실행되지 않고, deps가 변경될 때에만 실행됩니다.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const useDidMountEffect = (func: any, deps: any) => {
+const useDidMountEffect = (
+  effect: React.EffectCallback,
+  deps: React.DependencyList,
+) => {
   const didMount = useRef(false);
 
   useEffect(() => {
-    if (didMount.current) func();
+    if (didMount.current) effect();
     else didMount.current = true;
   }, deps);
 };
 
 export default useDidMountEffect;
+
+// create usedidmounteffect
+// Path: packages/hooks/index.ts
+export { default as useDidMountEffect } from './useDidMountEffect';
