c44279c8a02a8165c29b5b8f7db4437cf90ed499
neko113 <alstn113@gmail.com>
2022-11-01 21:26:44 +0900

    feat: add accordion storybook default, allowMutiple (#37)

--- packages/components/Accordion/Accordion.stories.tsx
+++ packages/components/Accordion/Accordion.stories.tsx
@@ -9,20 +9,58 @@ export default {
 
 export const Default = () => (
   <Accordion>
-    <Accordion.Item label="Item 1">
+    <Accordion.Item label="Item 1" description="description">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
       dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
-      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos?
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad, dolore
+      harum facilis sequi recusandae fugiat, delectus iste id libero! Quasi,
+      nulla sapiente illo similique culpa asperiores quisquam quos?
     </Accordion.Item>
-    <Accordion.Item label="Item 2">
+    <Accordion.Item label="Item 2" description="description">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
       dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
-      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos?
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad, dolore
+      harum facilis sequi recusandae fugiat, delectus iste id libero! Quasi,
+      nulla sapiente illo similique culpa asperiores quisquam quos?
     </Accordion.Item>
-    <Accordion.Item label="Item 3">
+    <Accordion.Item label="Item 3" description="description">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
       dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
-      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos?
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad, dolore
+      harum facilis sequi recusandae fugiat, delectus iste id libero! Quasi,
+      nulla sapiente illo similique culpa asperiores quisquam quos?
+    </Accordion.Item>
+  </Accordion>
+);
+
+export const allowMultiple = () => (
+  <Accordion allowMultiple>
+    <Accordion.Item label="Item 1" description="description">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
+      dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad, dolore
+      harum facilis sequi recusandae fugiat, delectus iste id libero! Quasi,
+      nulla sapiente illo similique culpa asperiores quisquam quos?
+    </Accordion.Item>
+    <Accordion.Item label="Item 2" description="description">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
+      dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad, dolore
+      harum facilis sequi recusandae fugiat, delectus iste id libero! Quasi,
+      nulla sapiente illo similique culpa asperiores quisquam quos?
+    </Accordion.Item>
+    <Accordion.Item label="Item 3" description="description">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad,
+      dolore harum facilis sequi recusandae fugiat, delectus iste id libero!
+      Quasi, nulla sapiente illo similique culpa asperiores quisquam quos? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Iusto et ad, dolore
+      harum facilis sequi recusandae fugiat, delectus iste id libero! Quasi,
+      nulla sapiente illo similique culpa asperiores quisquam quos?
     </Accordion.Item>
   </Accordion>
 );
