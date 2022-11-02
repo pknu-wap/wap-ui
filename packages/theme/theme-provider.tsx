507cb54fc8c6b1181ad1b9d3a14aba6f1c33ab80
neko113 <alstn113@gmail.com>
2022-11-02 22:38:26 +0900

    refactor: wap-ui-provider types (#46)

--- packages/theme/theme-provider.tsx
+++ packages/theme/theme-provider.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { ThemeProvider, ThemeProviderProps } from '@emotion/react';
+import { Theme, ThemeProvider } from '@emotion/react';
 import lightTheme from './light-theme';
 
-type Props = ThemeProviderProps;
+interface Props {
+  theme?: Theme;
+  children: React.ReactNode;
+}
 
 export const WapUIProvider = ({ children, theme = lightTheme }: Props) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
