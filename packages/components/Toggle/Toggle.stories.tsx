b5f616ae1e519d6095e45d13514d2031a474d1a1
neko113 <alstn113@gmail.com>
2022-11-03 10:42:14 +0900

    feat: create toggle (#44)

---
+++ packages/components/Toggle/Toggle.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Spacer } from '../../layouts';
+import { Toggle, Props } from './Toggle';
+
+export default {
+  title: 'Components/Toggle',
+  component: Toggle,
+} as ComponentMeta<typeof Toggle>;
+
+const Template: ComponentStory<typeof Toggle> = (args: Props) => {
+  return (
+    <FlexColumn>
+      <Toggle {...args} />
+      <Spacer y={2} />
+      <Toggle labelText="primary" color="primary" defaultChecked />
+      <Spacer />
+      <Toggle labelText="error" color="error" defaultChecked />
+      <Spacer />
+      <Toggle labelText="secondary" color="secondary" defaultChecked />
+      <Spacer />
+      <Toggle labelText="success" color="success" defaultChecked />
+      <Spacer />
+      <Toggle labelText="warning" color="warning" defaultChecked />
+    </FlexColumn>
+  );
+};
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  labelText: 'default',
+};
