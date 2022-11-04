ea2dfd414fd0c773b7099885ecae40eb84e572a9
neko113 <alstn113@gmail.com>
2022-11-04 13:49:25 +0900

    fix: useDisclosure comment (#50)

--- packages/hooks/useDisclosure.ts
+++ packages/hooks/useDisclosure.ts
@@ -5,9 +5,9 @@ interface Props {
 }
 
 /**
- * [열린상태, 열기, 닫기, 토글] 의 기능을 가진다.
+ * {열린상태, 열기, 닫기, 토글} 의 기능을 가진다.
  * @example
- * const [isOpen, open, close, toggle] = useDisclosure();
+ * const {isOpen, open, close, toggle} = useDisclosure();
  */
 
 const useDisclosure = ({ defaultIsOpen = false }: Props = {}) => {
