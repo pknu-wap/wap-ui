be94ab40c8d4c0d0fdc8fc4ca1f057e9d9d406fa
neko113 <alstn113@gmail.com>
2022-11-04 14:04:22 +0900

    fix: hooks export 방법 변경 (#50)

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,5 +1,5 @@
-export * from './useDidMountEffect';
-export * from './useDisclosure';
-export * from './useThrottle';
-export * from './useDebounce';
-export * from './useIntersectionObserver';
+export { default as useDidMountEffect } from './useDidMountEffect';
+export { default as useDisclosure } from './useDisclosure';
+export { default as useThrottle } from './useThrottle';
+export { default as useDebounce } from './useDebounce';
+export { default as useIntersectionObserver } from './useIntersectionObserver';
