eea4f3d79267eb1aeb4fbffa814c016a7164b753
neko113 <alstn113@gmail.com>
2022-11-04 13:47:32 +0900

    feat: add useIntersectionObserver hook (#50)

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './useDidMountEffect';
 export * from './useDisclosure';
 export * from './useThrottle';
 export * from './useDebounce';
+export * from './useIntersectionObserver';
--- packages/hooks/useDebounce.ts
+++ packages/hooks/useDebounce.ts
@@ -9,6 +9,16 @@ interface Props<T> {
  * useDebounce hook은 delay 안에 값 변경 시 값을 유지한다.
  * delay 안에 함수가 한번더 실행되면 앞의 작업을 취소하고 delay이
  * 지날 때까지 다시 호출 되지 않으면 callback을 실행하는 형식으로 되어있다.
+ *
+ * @example
+ * const [searchText, setSearchText] = useState<string>('');
+ * const debouncedText = useDebounce<string>({ value: searchText });
+ * <TextInput
+ *   placeholder="Search"
+ *   type="text"
+ *   value={searchText}
+ *   onChange={(e) => setSearchText(e.target.value)}
+ * />
  */
 const useDebounce = <T>({ value, delay = 500 }: Props<T>): T => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
