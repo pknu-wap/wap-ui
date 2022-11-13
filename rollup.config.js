ff1694b9bb8d07f6b2fe4f293ce17376f8caeceb
neko113 <alstn113@gmail.com>
2022-11-14 01:35:12 +0900

    feat: 번들링을 최소화하기 위한 terser플러그인 추가 (#55)

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// rollup-plugin-terser: 생성된 es번들을 최소화하기 위한 플러그인
+import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['js', 'jsx', 'ts', 'tsx', 'mjs'];
 import pkg from './package.json';
@@ -33,5 +35,6 @@ export default {
     commonjs({ include: 'node_modules/**' }),
     peerDepsExternal(),
     typescript({ tsconfig: './tsconfig.json', sourceMap: true }),
+    terser(),
   ],
 };
