0b900ccc71927810b0c1c33fff1966cc41e59c6f
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-11-18 14:37:39 +0900

    feat: set bars size (#59)

--- packages/components/Loader/Loaders/Bars/Bars.styles.ts
+++ packages/components/Loader/Loaders/Bars/Bars.styles.ts
@@ -21,19 +21,29 @@ export const Container = styled.div<{ size: 'sm' | 'md' | 'lg' }>`
   ${({ size }) =>
     size === 'sm'
       ? css`
-          height: 1.375rem;
+          width: 1.75rem;
+          height: 0.875rem;
+          div {
+            width: 0.25rem;
+            height: 100%;
+          }
         `
       : size === 'md'
       ? css`
+          width: 2.7rem;
+          height: 1.375rem;
+          div {
+            width: 0.375rem;
+            height: 100%;
+          }
+        `
+      : css`
           width: 4.5rem;
           height: 2.25rem;
           div {
             width: 0.6rem;
             height: 100%;
           }
-        `
-      : css`
-          height: 2.75rem;
         `};
 `;
 
