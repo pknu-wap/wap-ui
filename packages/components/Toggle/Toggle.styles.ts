494aa9451e51d550fc96652bc9e3bc79897addd0
김민수 MINSOO KIM <alstn113@gmail.com>
2022-11-30 01:30:08 +0900

    feat: toggle size variant (#56)

--- packages/components/Toggle/Toggle.styles.ts
+++ packages/components/Toggle/Toggle.styles.ts
@@ -1,50 +1,106 @@
 import { NormalColorType } from '../../theme/types';
 import { palette } from '../../theme/palette';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
-export const ToggleLabel = styled.label`
+export const ToggleLabel = styled.label<{ variant: 'sm' | 'lg' }>`
   display: flex;
   align-items: center;
-  gap: 0.5rem;
+
+  ${({ variant }) =>
+    variant === 'sm' &&
+    css`
+      gap: 0.5rem;
+    `}
+  ${({ variant }) =>
+    variant === 'lg' &&
+    css`
+      gap: 1rem;
+    `}
 `;
 
-export const ToggleSwitch = styled.span`
+export const ToggleSwitch = styled.span<{ variant: 'sm' | 'lg' }>`
   position: relative;
-  width: 2.5rem;
-  height: 1.5rem;
-  border-radius: 1rem;
   display: flex;
   align-items: center;
   background-color: ${palette.gray};
   cursor: pointer;
   transition: background-color 0.2s ease;
+
+  ${({ variant }) =>
+    variant === 'sm' &&
+    css`
+      width: 2.5rem;
+      height: 1.5rem;
+      border-radius: 1rem;
+    `}
+  ${({ variant }) =>
+    variant === 'lg' &&
+    css`
+      width: 5rem;
+      height: 3rem;
+      border-radius: 2rem;
+    `}
+
   &::after {
     content: '';
     position: absolute;
     display: inline-block;
-    background-color: ${palette.white};
-    width: 1rem;
-    height: 1rem;
     border-radius: 50%;
+    background-color: ${palette.white};
     transition: transform 0.2s ease, background-color 0.2s ease;
+
+    ${({ variant }) =>
+      variant === 'sm' &&
+      css`
+        width: 1rem;
+        height: 1rem;
+      `}
+    ${({ variant }) =>
+      variant === 'lg' &&
+      css`
+        width: 2rem;
+        height: 2rem;
+      `}
   }
 `;
 
-export const ToggleText = styled.span`
-  font-size: 1rem;
-  line-height: 1rem;
+export const ToggleText = styled.span<{ variant: 'sm' | 'lg' }>`
   user-select: none;
+
+  ${({ variant }) =>
+    variant === 'sm' &&
+    css`
+      font-size: 1rem;
+      line-height: 1rem;
+    `}
+  ${({ variant }) =>
+    variant === 'lg' &&
+    css`
+      font-size: 2rem;
+      line-height: 2rem;
+    `}
 `;
 
 export const ToggleCheckbox = styled.input<{
   color: NormalColorType;
+  variant: 'sm' | 'lg';
 }>`
   display: none;
 
   // Switch Off
   & ~ ${ToggleSwitch} {
     &::after {
-      transform: translateX(0.3rem);
+      ${({ variant }) =>
+        variant === 'sm' &&
+        css`
+          transform: translateX(0.3rem);
+        `}
+      ${({ variant }) =>
+        variant === 'lg' &&
+        css`
+          transform: translateX(0.6rem);
+        `}
     }
   }
 
@@ -53,7 +109,16 @@ export const ToggleCheckbox = styled.input<{
     & ~ ${ToggleSwitch} {
       background-color: ${({ color }) => palette[color]};
       &::after {
-        transform: translateX(1.2rem);
+        ${({ variant }) =>
+          variant === 'sm' &&
+          css`
+            transform: translateX(1.2rem);
+          `}
+        ${({ variant }) =>
+          variant === 'lg' &&
+          css`
+            transform: translateX(2.4rem);
+          `}
       }
     }
   }
--- packages/components/Toggle/Toggle.tsx
+++ packages/components/Toggle/Toggle.tsx
@@ -5,6 +5,7 @@ import * as S from './Toggle.styles';
 export interface ToggleProps extends InputHTMLAttributes<HTMLInputElement> {
   labelText?: string;
   color?: NormalColorType;
+  variant?: 'sm' | 'lg';
 }
 
 /**
@@ -16,16 +17,30 @@ export interface ToggleProps extends InputHTMLAttributes<HTMLInputElement> {
  * <Toggle labelText="Toggle" color="warning" />
  * <Toggle labelText="Toggle" color="error" />
  * ```
+ * @example
+ * ```tsx
+ * <Toggle labelText="Toggle" color="primary" variant="lg" />
+ * <Toggle labelText="Toggle" color="success" variant="lg" />
+ * <Toggle labelText="Toggle" color="secondary" variant="lg" />
+ * <Toggle labelText="Toggle" color="warning" variant="lg" />
+ * <Toggle labelText="Toggle" color="error" variant="lg" />
+ * ```
  */
 export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(function Toggle(
-  { labelText = '', color = 'primary', ...options },
+  { labelText = '', color = 'primary', variant = 'sm', ...options },
   ref,
 ) {
   return (
-    <S.ToggleLabel>
-      <S.ToggleText>{labelText}</S.ToggleText>
-      <S.ToggleCheckbox type="checkbox" ref={ref} color={color} {...options} />
-      <S.ToggleSwitch />
+    <S.ToggleLabel variant={variant}>
+      <S.ToggleText variant={variant}>{labelText}</S.ToggleText>
+      <S.ToggleCheckbox
+        type="checkbox"
+        variant={variant}
+        ref={ref}
+        color={color}
+        {...options}
+      />
+      <S.ToggleSwitch variant={variant} />
     </S.ToggleLabel>
   );
 });
