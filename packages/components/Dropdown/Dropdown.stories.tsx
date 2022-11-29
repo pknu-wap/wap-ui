f750c659fac1c18b2d085f47dd3315244fbee6ce
김민수 MINSOO KIM <alstn113@gmail.com>
2022-11-29 21:49:16 +0900

    refactor: dropdown storybook (#56)

--- packages/components/Dropdown/Dropdown.stories.tsx
+++ packages/components/Dropdown/Dropdown.stories.tsx
@@ -12,34 +12,63 @@ export default {
 
 export const Defulat = () => {
   return (
-    <FlexRow>
-      <Spacer x={3} />
-      <Dropdown>
-        <Dropdown.Button shadow color="error">
-          Actions
-        </Dropdown.Button>
-        <Dropdown.Menu>
-          <Dropdown.MenuItem>First Action</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Second Action</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Third Acition</Dropdown.MenuItem>
-        </Dropdown.Menu>
-      </Dropdown>
-      <Spacer x={10} />
-      <Dropdown>
-        <Dropdown.Button shadow color="success">
-          Actions
-        </Dropdown.Button>
-        <Dropdown.Menu>
-          <Dropdown.MenuItem onClick={() => console.log('sdf')}>
-            Create a Copy
-          </Dropdown.MenuItem>
-          <Dropdown.MenuItem>Download</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Delete</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Mark as Draft</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Write Something</Dropdown.MenuItem>
-        </Dropdown.Menu>
-      </Dropdown>
-    </FlexRow>
+    <FlexColumn>
+      <FlexRow>
+        <Spacer x={3} />
+        <Dropdown>
+          <Dropdown.Button shadow color="primary">
+            Actions
+          </Dropdown.Button>
+          <Dropdown.Menu>
+            <Dropdown.MenuItem>First Action</Dropdown.MenuItem>
+            <Dropdown.MenuItem>Second Action</Dropdown.MenuItem>
+            <Dropdown.MenuItem>Third Acition</Dropdown.MenuItem>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Spacer x={10} />
+        <Dropdown>
+          <Dropdown.Button shadow color="warning">
+            Actions
+          </Dropdown.Button>
+          <Dropdown.Menu>
+            <Dropdown.MenuItem onClick={() => console.log('sdf')}>
+              Create a Copy
+            </Dropdown.MenuItem>
+            <Dropdown.MenuItem>Download</Dropdown.MenuItem>
+            <Dropdown.MenuItem>Delete</Dropdown.MenuItem>
+            <Dropdown.MenuItem>Mark as Draft</Dropdown.MenuItem>
+            <Dropdown.MenuItem>Write Something</Dropdown.MenuItem>
+          </Dropdown.Menu>
+        </Dropdown>
+      </FlexRow>
+      <Spacer y={14} />
+      <FlexRow>
+        <Spacer x={3} />
+        <Dropdown>
+          <Dropdown.Button shadow color="error">
+            Actions
+          </Dropdown.Button>
+          <Dropdown.Menu>
+            <Dropdown.MenuItem>First Action</Dropdown.MenuItem>
+            <Dropdown.MenuItem>Second Action</Dropdown.MenuItem>
+            <Dropdown.MenuItem>Third Acition</Dropdown.MenuItem>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Spacer x={10} />
+        <Dropdown>
+          <Dropdown.Button shadow color="secondary">
+            Actions
+          </Dropdown.Button>
+          <Dropdown.Menu>
+            <Dropdown.MenuItem onClick={() => console.log('sdf')}>
+              Create a Copy
+            </Dropdown.MenuItem>
+            <Dropdown.MenuItem>Download</Dropdown.MenuItem>
+            <Dropdown.MenuItem>Delete</Dropdown.MenuItem>
+          </Dropdown.Menu>
+        </Dropdown>
+      </FlexRow>
+    </FlexColumn>
   );
 };
 
@@ -47,5 +76,10 @@ const FlexRow = styled.div`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
-  width: 1000px;
+`;
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
