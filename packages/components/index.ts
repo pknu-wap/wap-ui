9fad35975b23e43552b88b85724cee6756c22bec
김민수 MINSOO KIM <alstn113@gmail.com>
2022-11-30 17:39:19 +0900

    fix: remove tooltip component 포기... (#68)

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -7,8 +7,6 @@ export * from './Portal';
 export * from './Accordion';
 export * from './TextInput';
 export * from './Toggle';
-export * from './Tooltip';
 export * from './Loader';
 export * from './Toast';
 export * from './Dropdown';
-export * from './Tooltip';
