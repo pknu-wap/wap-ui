efdb555c508846eb8816a36d0abc0ecb77403f09
김민수 MINSOO KIM <alstn113@gmail.com>
2022-12-20 02:10:03 +0900

    feat: dropdown item color storybook 추가 (#77)

--- packages/components/Dropdown/Dropdown.stories.tsx
+++ packages/components/Dropdown/Dropdown.stories.tsx
@@ -10,83 +10,145 @@ export default {
   component: DropdownButton,
 } as ComponentMeta<typeof DropdownButton>;
 
-const RowSet = () => {
+export const Defulat = () => {
   return (
-    <FlexRow>
+    <>
       <Dropdown>
-        <Dropdown.Button shadow color="primary">
-          Actions
-        </Dropdown.Button>
+        <Dropdown.Button shadow>Actions</Dropdown.Button>
         <Dropdown.Menu>
+          <Dropdown.MenuItem onAction={() => console.log('sdf')}>
+            Create a Copy
+          </Dropdown.MenuItem>
+          <Dropdown.MenuItem>Download</Dropdown.MenuItem>
+          <Dropdown.MenuItem>Mark as Draft</Dropdown.MenuItem>
+          <Dropdown.MenuItem>Write Something</Dropdown.MenuItem>
           <Dropdown.MenuItem
-            onAction={() => {
-              alert('asdfv');
-            }}
+            onAction={() => alert('Delete Something')}
             color="error"
           >
-            First Action
+            Delete
           </Dropdown.MenuItem>
-          <Dropdown.MenuItem>Second Action</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Third Acition</Dropdown.MenuItem>
         </Dropdown.Menu>
       </Dropdown>
-      <Spacer x={10} />
-      <Dropdown>
-        <Dropdown.Button shadow color="warning">
-          Actions
-        </Dropdown.Button>
-        <Dropdown.Menu>
-          <Dropdown.MenuItem onAction={() => console.log('sdf')}>
-            Create a Copy
-          </Dropdown.MenuItem>
-          <Dropdown.MenuItem>Download</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Delete</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Mark as Draft</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Write Something</Dropdown.MenuItem>
-        </Dropdown.Menu>
-      </Dropdown>
-      <Spacer x={10} />
-      <Dropdown>
-        <Dropdown.Button shadow color="error">
-          Actions
-        </Dropdown.Button>
-        <Dropdown.Menu>
-          <Dropdown.MenuItem
-            onAction={() => {
-              alert('sdf');
-            }}
-          >
-            First Action
-          </Dropdown.MenuItem>
-          <Dropdown.MenuItem>Second Action</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Third Acition</Dropdown.MenuItem>
-        </Dropdown.Menu>
-      </Dropdown>
-      <Spacer x={10} />
-      <Dropdown>
-        <Dropdown.Button shadow color="secondary">
-          Actions
-        </Dropdown.Button>
-        <Dropdown.Menu>
-          <Dropdown.MenuItem onAction={() => console.log('sdf')}>
-            Create a Copy
-          </Dropdown.MenuItem>
-          <Dropdown.MenuItem>Download</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Delete</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Mark as Draft</Dropdown.MenuItem>
-          <Dropdown.MenuItem>Write Something</Dropdown.MenuItem>
-        </Dropdown.Menu>
-      </Dropdown>
-    </FlexRow>
+    </>
   );
 };
-export const Defulat = () => {
+
+export const variousLocation = () => {
   return (
     <Container>
       <FlexColumn>
-        <RowSet />
-        <Spacer y={10} />
-        <RowSet />
+        <FlexRow>
+          <Dropdown>
+            <Dropdown.Button shadow>Actions</Dropdown.Button>
+            <Dropdown.Menu>
+              <Dropdown.MenuItem>Create a Copy</Dropdown.MenuItem>
+              <Dropdown.MenuItem
+                onAction={() => alert('Download Something')}
+                color="primary"
+              >
+                Download
+              </Dropdown.MenuItem>
+              <Dropdown.MenuItem>Mark as Draft</Dropdown.MenuItem>
+              <Dropdown.MenuItem>Write Something</Dropdown.MenuItem>
+              <Dropdown.MenuItem
+                onAction={() => alert('Delete Something')}
+                color="error"
+              >
+                Delete
+              </Dropdown.MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Spacer x={10} />
+          <Dropdown>
+            <Dropdown.Button shadow color="error">
+              Actions
+            </Dropdown.Button>
+            <Dropdown.Menu>
+              <Dropdown.MenuItem>Create a Copy</Dropdown.MenuItem>
+              <Dropdown.MenuItem>Download</Dropdown.MenuItem>
+              <Dropdown.MenuItem>Mark as Draft</Dropdown.MenuItem>
+              <Dropdown.MenuItem>Write Something</Dropdown.MenuItem>
+              <Dropdown.MenuItem
+                onAction={() => alert('Delete Something')}
+                color="error"
+              >
+                Delete
+              </Dropdown.MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Spacer x={10} />
+          <Dropdown>
+            <Dropdown.Button shadow color="success">
+              Actions
+            </Dropdown.Button>
+            <Dropdown.Menu>
+              <Dropdown.MenuItem>Create a Copy</Dropdown.MenuItem>
+              <Dropdown.MenuItem>Download</Dropdown.MenuItem>
+              <Dropdown.MenuItem
+                onAction={() => alert('Mark as Draft Something')}
+                color="success"
+              >
+                Mark as Draft
+              </Dropdown.MenuItem>
+              <Dropdown.MenuItem>Write Something</Dropdown.MenuItem>
+              <Dropdown.MenuItem
+                onAction={() => alert('Delete Something')}
+                color="error"
+              >
+                Delete
+              </Dropdown.MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+        </FlexRow>
+        <Spacer y={16} />
+        <FlexRow>
+          <Dropdown>
+            <Dropdown.Button shadow color="secondary">
+              Actions
+            </Dropdown.Button>
+            <Dropdown.Menu>
+              <Dropdown.MenuItem
+                onAction={() => alert('Booooommmm!!!')}
+                color="secondary"
+              >
+                First Action
+              </Dropdown.MenuItem>
+              <Dropdown.MenuItem>Second Action</Dropdown.MenuItem>
+              <Dropdown.MenuItem>Third Action</Dropdown.MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Spacer x={10} />
+          <Dropdown>
+            <Dropdown.Button shadow color="warning">
+              Actions
+            </Dropdown.Button>
+            <Dropdown.Menu>
+              <Dropdown.MenuItem
+                onAction={() => alert('Boooomm!!!')}
+                color="warning"
+              >
+                First Action
+              </Dropdown.MenuItem>
+              <Dropdown.MenuItem>Second Action</Dropdown.MenuItem>
+              <Dropdown.MenuItem>Third Action</Dropdown.MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Spacer x={10} />
+          <Dropdown>
+            <Dropdown.Button shadow>Actions</Dropdown.Button>
+            <Dropdown.Menu>
+              <Dropdown.MenuItem
+                onAction={() => alert('Boooooom!')}
+                color="primary"
+              >
+                First Action
+              </Dropdown.MenuItem>
+              <Dropdown.MenuItem>Second Action</Dropdown.MenuItem>
+              <Dropdown.MenuItem>Third Action</Dropdown.MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+        </FlexRow>
       </FlexColumn>
     </Container>
   );
