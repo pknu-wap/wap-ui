56dfea8358694864306c2c7cfd27963b6d06ad33
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-28 12:28:34 +0900

    refactor: use portal cleanup

--- packages/react/src/hooks/usePortal.ts
+++ packages/react/src/hooks/usePortal.ts
@@ -42,7 +42,9 @@ const usePortal = (selectId: string): HTMLElement | null => {
     setElSnapshot(el);
 
     return () => {
-      parentElement.removeChild(el);
+      if (el.parentElement) {
+        el.parentElement.removeChild(el);
+      }
     };
     // Id가 변경될 일이 없으므로 빈 배열을 넣어준다.
     // eslint-disable-next-line react-hooks/exhaustive-deps
